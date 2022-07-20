d46ffd9bfdf88b429fa149ca49fc961ec53ab57f
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2022-07-20 11:48:50 +0530

    Add files via upload

---
+++ Display_number.cpp
@@ -0,0 +1,17 @@
+//Write a program to print a given number using function
+
+#include<iostream>
+using namespace std;
+void display(int n1)
+{
+    cout<<n1<<endl;
+    return ;
+}
+int main()
+{
+    int a;
+    cout<<"enter the number to be display";
+    cin>>a;
+    display(a);
+    return 0;
+}
