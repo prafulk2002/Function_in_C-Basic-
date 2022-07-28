e35c8bd884ad8cb0183d619d0fad9b74a6282ab3
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2022-07-28 17:45:04 +0530

    Add files via upload

---
+++ Primeno.cpp
@@ -0,0 +1,28 @@
+// Write a program to print prime numbers using functions.
+
+#include<iostream>
+using namespace std;
+bool isprime(int num)
+{
+    for(int i=2; i<num/2; i++)
+    {
+        if(num%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    for(int i=a; i<b; i++)
+    {
+        if(isprime(i))
+        {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
