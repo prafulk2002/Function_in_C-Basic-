cc6c6ae24f459f9d20a05e09307091f62c65cb66
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2022-07-28 17:45:15 +0530

    Add files via upload

---
+++ Sum_of_n_naturalno.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int sum(int n)
+{
+    int ans=0;
+    for(int i=0; i<=n; i++)
+    {
+        ans=ans+i;
+    }
+    return ans;
+}
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<sum(a);
+    return 0;
+}
